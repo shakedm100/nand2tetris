1699439b0e673d9d394911b1e25064c7df89c7ce
shakedm100 <shaked1mi@gmail.com>
2024-05-27 20:45:48 +0300

    Week 4
    Finished week 4

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -8,4 +8,58 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+//Wait for Keyboard input, if input isn't zero then it paints black
+(LOOP)
+	@KBD
+	D=M
+	
+	@color
+	M=0
+	@i
+	M=0
+	
+	@PAINT
+	D;JEQ
+	
+	//If a click on the keyboard registered, paint black
+	@color
+	M=-1
+	
+//Paints black all the screen
+//Reminder, -1 = 111111111... and 1 = black pixel on screen
+
+(PAINT)
+	
+	//Stopping condition when i - 8191 == 0
+	@8191
+	D=A
+	@i
+	D=M-D
+	
+	@LOOP
+	D;JEQ
+	
+	//Point to Screen + i
+	@i
+	D=M
+	@SCREEN
+	D=A+D
+	
+	//Paint by @color
+	@pointer
+	M=D
+	@color
+	D=M
+	@pointer
+	A=M
+	M=D
+	
+	//Increase i by one
+	@i
+	M=M+1
+	
+	@PAINT
+	1;JMP
+	
+	
--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -7,4 +7,45 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@sum
+M=0
+
+@R1
+D=M
+@i
+M=D
+
+ (LOOP)
+	
+	//Check if stop condition met
+	@i
+	D=M
+	@STOP
+	D;JEQ
+	
+	//Add to sum
+	@R0
+	D=M
+	@sum
+	M=M+D
+	
+	//Decrease i
+	@i
+	M=M-1
+	
+	//Loop back
+	@LOOP
+	1;JMP
+
+(STOP)
+	//Add sum to R2
+	@sum
+	D=M
+	@R2
+	M=D
+
+//Loop the program at exit
+(END)
+	@END
+	1;JMP
+	
